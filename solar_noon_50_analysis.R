db587bd381cf6ed65a739deecef8605a3dc86688
chriscie <christine.m.cieslak@gmail.com>
2025-11-14 15:46:45 -0500

    Added pure permutation test.

--- solar_noon_50_analysis.R
+++ solar_noon_50_analysis.R
@@ -56,6 +56,15 @@ z_value
 
 z_value/sqrt(length(covered_values)+length(uncovered_values)) 
 
+# Wilcoxon rank-sum test using Monte Carlo simulation for P-value ("pure" permutation)
+# This is the "pure" permutation approach
+test.perm <- wilcox_test(value ~ group, data = data, distribution = "approximate",n = 10000) 
+
+# Use a large number of permutations for stability
+
+# See results
+print(test.perm)
+
 #Graph showing hatch success of covered and uncovered nests
 #load graph package
 library(ggplot2)
